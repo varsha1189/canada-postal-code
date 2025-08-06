a882bc4f84b4a759b9eebf12680fa283eaa1b830
varsha1189 <11.varsha.m@gmail.com>
2025-08-06 14:01:06 -0500

    Update canadian_zip_app_py.py

--- canadian_zip_app_py.py
+++ canadian_zip_app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1O5JpVIwyQjmMJGmJZQ9n3VhrNJ2ywY66
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
